b4a72ba626c150a124b541c036c6eeff77ec738a
Andrea Botello <abotellod@gmail.com>
2026-09-24 11:56:58 -0500

    Hall. Web [1.2]

--- src/config/global.js
+++ src/config/global.js
@@ -378,8 +378,8 @@ export default {
           centro: 'Centro Agroturístico - Regional Santander',
         },
         {
-          nombre: ' ',
-          cargo: 'Animador y productor audiovisual',
+          nombre: 'Maria Alejandra Vera Briceño',
+          cargo: 'Animadora y productora audiovisual',
           centro: 'Centro Agroturístico - Regional Santander',
         },
       ],
